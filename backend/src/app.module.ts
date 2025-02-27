a49876af6a4c4fa4eaa20088c131cee3e4a0c843
Camilo Gomez <camigomezbernal@gmail.com>
2025-02-27 09:35:25 -0500

    Se Dockeriza backend y se crea compose.yml para conectar db, queda funcional

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,8 +9,8 @@ import { PatientsModule } from './patients/patients.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || '192.168.48.1', // 
-      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      host: process.env.DB_HOST, // 
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
